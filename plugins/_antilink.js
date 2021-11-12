8dd5bf8028c6b6cbb262e0fb376d0426df1b5960
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-12 21:39:32 +0700

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -10,9 +10,9 @@ handler.before = async function (m, { isAdmin, isBotAdmin }) {
     let thisGroup = `https://chat.whatsapp.com/${await conn.groupInviteCode(m.chat)}`
     if (m.text.includes(thisGroup)) throw false // jika link grup itu sendiri gak dikick
     await this.sendButon(m.chat, `*Link Grup Terdeteksi!*${isBotAdmin ? '' : '\n\nbukan admin jadi gabisa kick t_t'}\n\nKetik *.off antilink* untuk mematikan fitur ini${opts['restrict'] ? '' : '\nketik *#on restrict* supaya bisa kick'}`, '© stikerin', 'Matikan Antilink', ',0 antilink', m)
-    if (global.opts['restrict']) {
+    //if (global.opts['restrict']) {
       if (isBotAdmin) this.groupRemove(m.chat, [m.sender])
-    }
+    //}
   }
   return true
 }
