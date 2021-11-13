9e1b32f78c924dc9743887c5be454c0258fdfb47
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-14 06:49:05 +0700

    Add files via upload

---
+++ plugins/bannedList.js
@@ -0,0 +1,33 @@
+let handler = async (m, { conn, isOwner }) => {
+    let chats = Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned)
+    let users = Object.entries(global.db.data.users).filter(user => user[1].banned)
+    let caption = `
+╭━━•✵ ⃟  ⃟  ⃟✵•
+┃
+┃ *Chat banned : ↷↷*
+┃
+┃ *⸙ ુོ➪ Total : ${chats.length} Chat${chats ? '\n' + chats.map(([jid], i) => `
+┃ *〲 ${i + 1}. ${conn.getName(jid) == undefined ? 'Unknown' : conn.getName(jid)}
+┃ *〲 ${isOwner ? '@' + jid.split`@`[0] : jid}
+`.trim()).join('\n') : ''}
+┃ *─★‧ﾟ
+╰━━━━━━━━⸙
+
+╭━━•✵ ⃟  ⃟  ⃟✵•
+┃
+┃ *Pengguna Banned : ↷↷*
+┃
+┃ *⸙ ુོ➪ Total : ${users.length} Pengguna${users ? '\n' + users.map(([jid], i) => `
+┃ *〲 ${i + 1}. ${conn.getName(jid) == undefined ? 'Unknown' : conn.getName(jid)}
+┃ *〲 ${isOwner ? '@' + jid.split`@`[0] : jid}
+`.trim()).join('\n') : ''}
+┃ *─★‧ﾟ
+╰━━━━━━━━⸙
+`.trim()
+    conn.reply(m.chat, caption, m, { contextInfo: { mentionedJid: conn.parseMention(caption) } })
+}
+handler.help = ['bannedlist']
+handler.tags = ['info']
+handler.command = /^listban(ned)?|ban(ned)?list|daftarban(ned)?$/i
+
+module.exports = handler
