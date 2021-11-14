baa10f0b0a5e52e852dabc0c9c7beb78993e28cf
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-14 14:14:52 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -7,6 +7,7 @@ let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (awa
 ⁙┃ ુོ DANA : 6285346545126
 ⁙┃ ુོ OVO    : 6285346545126
 ⁙┃ ુོ Gopay : 6285346545126
+⁙┃ ુོ PULSA : 6285346545126
 ⁙┃ ુོ https://saweria.co/raselganz
 ⁙┃ ુོ https://trakteer.id/raselganz/tip
 ⁙┃ ુོ https://instagram.com/rasel.ganz
