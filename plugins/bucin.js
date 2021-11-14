a904a9e359a1d2a87bd4f12e617f5611ab9573ce
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-14 07:45:24 +0700

    Update bucin.js

--- plugins/bucin.js
+++ plugins/bucin.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
-  await conn.sendButton(m.chat, `“${pickRandom(global.bucin)}”`, watermark, 'Bucin', `${usedPrefix + command}`, m)
+  await conn.sendButton(m.chat, `“${pickRandom(global.bucin)}”`, watermark, 'Bucin', `${usedPrefix + command}`)
 }
 handler.help = ['bucin']
 handler.tags = ['quotes']
