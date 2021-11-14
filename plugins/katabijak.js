8d64923cdd37fb360f7a65f5364a7105a573f0ca
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-15 04:46:02 +0700

    Update katabijak.js

--- plugins/katabijak.js
+++ plugins/katabijak.js
@@ -33,7 +33,7 @@ let handler = async (m, { command, args, usedPrefix }) => {
                 let random = Math.floor(Math.random() * json.data.length)
                 let hasil = json.data[random]
                 let { author, bio, quote } = hasil
-                await conn.send2Button(m.chat, `“${quote}”`, `${author} - ${bio}`, `KATA BIJAK ${args[0].toUpperCase()}`, `${usedPrefix + command} ${args[0]}`, `RANDOM`, `${usedPrefix + command} ${pickRandom(['rindu', 'mimpi', 'sendiri', 'sabar', 'kesedihan', 'pernikahan', 'kemerdekaan'])}`, m)
+                await conn.send2ButtonLoc(m.chat, await(await fetch(image)).buffer(), `“${quote}”`, `${author} - ${bio}`, `KATA BIJAK ${args[0].toUpperCase()}`, `${usedPrefix + command} ${args[0]}`, `RANDOM`, `${usedPrefix + command} ${pickRandom(['rindu', 'mimpi', 'sendiri', 'sabar', 'kesedihan', 'pernikahan', 'kemerdekaan'])}`, m)
             })
             break
         default:
