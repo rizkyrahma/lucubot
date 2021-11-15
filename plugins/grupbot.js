78e83f074346b5b795886d12e2939a7421937420
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-15 11:09:52 +0700

    Update grupbot.js

--- plugins/grupbot.js
+++ plugins/grupbot.js
@@ -1,6 +1,6 @@
 let { MessageType } = require('@adiwajshing/baileys')
 //let handler = async (m, { conn, text, usedPrefix }) => {
-let handler = async m => {    
+//let handler = async m => {    
 await conn.sendMessage(res.gid, `
 Grup whatsapp bot, jangan lupa join ya su<3
 `, MessageType.text, { contextInfo: { externalAdReply :{
@@ -11,7 +11,7 @@ Grup whatsapp bot, jangan lupa join ya su<3
        thumbnailUrl: image,
 sourceUrl: 'https://chat.whatsapp.com/CUCsW6BWfmJLJwJgPQIaKM'
 }}})
-}
+
 handler.help = ['groupbot']
 handler.tags = ['info', 'grup']
 handler.command = /^g(c|ro?up)bot$/i
