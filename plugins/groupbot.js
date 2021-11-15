3f0788dd00083866d80dedb12c48c28510d411fa
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-15 11:21:02 +0700

    Update groupbot.js

--- plugins/groupbot.js
+++ plugins/groupbot.js
@@ -3,11 +3,8 @@ let handler = async m => m.reply(`
 
 https://chat.whatsapp.com/CUCsW6BWfmJLJwJgPQIaKM
 
-\`\`\`Jangan lupa join ya kak ${m.sender} 🥰\`\`\`
+\`\`\`Jangan lupa join ya kak @${user.split('@')[0]} 🥰\`\`\`
 `.trim())
-contextInfo: {
-      mentionedJid: conn.m.sender
-  }
 handler.help = ['groupbot']
 handler.tags = ['info', 'grup']
 handler.command = /^(g(c|ro?up)bot)2$/i
