e6f3b7f88d100f9fa0e23aaaaa3d2a51ee5416ec
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-15 11:17:41 +0700

    Update groupbot.js

--- plugins/groupbot.js
+++ plugins/groupbot.js
@@ -5,6 +5,9 @@ https://chat.whatsapp.com/CUCsW6BWfmJLJwJgPQIaKM
 
 \`\`\`Jangan lupa join ya kak ${m.sender} 🥰\`\`\`
 `.trim())
+contextInfo: {
+      mentionedJid: conn.m.sender
+  }
 handler.help = ['groupbot']
 handler.tags = ['info', 'grup']
 handler.command = /^(g(c|ro?up)bot)2$/i
