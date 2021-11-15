fef27380f662a4b333f472f75a88041dca2baffa
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-15 07:08:39 +0700

    Update listmsg.js

--- plugins/listmsg.js
+++ plugins/listmsg.js
@@ -6,25 +6,31 @@ let handler = async (m, { usedPrefix, command }) => {
     if (/audio/i.test(command)) fltr = split
         .filter(v => v.message.audioMessage)
         .filter(m => m.message.audioMessage.ptt == false)
-        .map(v => '├ ' + v.nama).join('\n')
+        .map(v => '⁙┃〲*⸙ ' + v.nama).join('\n')
     if (/vn/i.test(command)) fltr = split
         .filter(v => v.message.audioMessage)
         .filter(m => m.message.audioMessage.ptt == true)
-        .map(v => '├ ' + v.nama).join('\n')
+        .map(v => '⁙┃〲*⸙ ' + v.nama).join('\n')
     if (/video/i.test(command)) fltr = split
         .filter(v => v.message.videoMessage && !v.message.videoMessage.gifPlayback)
-        .map(v => '├ ' + v.nama).join('\n')
+        .map(v => '⁙┃〲*⸙ ' + v.nama).join('\n')
     if (/gif/i.test(command)) fltr = split
         .filter(v => v.message.videoMessage)
         .filter(m => m.message.videoMessage.gifPlayback)
-        .map(v => '├ ' + v.nama).join('\n')
+        .map(v => '⁙┃〲*⸙ ' + v.nama).join('\n')
     if (/stic?ker/i.test(command)) fltr = split.filter(v => v.message.stickerMessage).map(v => '├ ' + v.nama).join('\n')
     if (/msg/i.test(command)) fltr = split.filter(v => v.message.conversation).map(v => '├ ' + v.nama).join('\n')
     if (/img/i.test(command)) fltr = split.filter(v => v.message.imageMessage).map(v => '├ ' + v.nama).join('\n')
     m.reply(`
-┌〔 LIST PESAN 〕
+╭━━•›ꪶ ཻུ۪۪ꦽꦼ̷⸙ ━ ━ ━ ━ ꪶ ཻུ۪۪ꦽꦼ̷⸙‹•━━╮
+┃╭┈─────────────⩵꙰ཱི࿐
+┃╰── *DAFTAR* ──➤ ↶↷
+╰•͙✩̣̣̣̣
+⁙┃〲  
 ${fltr}
-└────
+⁙┃〲 
+⁙╰•°°°🕊°°°°°🕊°°°°°°🕊°°°°°°°°
+
 Akses/ambil dengan mengetik:
 *${usedPrefix}get${which}* <nama>
 atau langsung tanpa perintah
