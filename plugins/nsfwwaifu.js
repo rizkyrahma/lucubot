7709edff64ba97fce124783fd11ffc7e072523ea
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-15 12:53:25 +0700

    Update nsfwwaifu.js

--- plugins/nsfwwaifu.js
+++ plugins/nsfwwaifu.js
@@ -4,8 +4,8 @@ let handler = async (m, { conn }) => {
   let res = await fetch(global.API("https://api.waifu.pics/", "/nsfw/waifu"));
   if (!res.ok) throw await `${res.status} ${res.statusText}`;
   let json = await res.json();
-  if (json.urlnsfwnsfwwaifu)
-    await conn.sendButtonImg(m.chat, await (await fetch(json.urlnsfwwaifu)).buffer(), 'Nekonya bwank', watermark, 'Get Again -limit', '/nsfwwaifu', m);
+  if (json.url)
+    await conn.sendButtonImg(m.chat, await (await fetch(json.url)).buffer(), 'Jangan sagne bwank, mau lagi?=limit', watermark, 'Get Again -limit', '/nsfwwaifu', m);
     //conn.sendFile(
       //m.chat,
       //json.url,
@@ -16,7 +16,7 @@ let handler = async (m, { conn }) => {
   else throw json;
 };
 handler.help = ["nsfwwaifu"];
-handler.tags = ["fun"];
+handler.tags = ["fun", "anime"];
 
 handler.command = /^(nsfwwaifu|waifunsfw)$/i;
 
