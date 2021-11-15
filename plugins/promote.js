30981015b5b9660b48c8690f3910a4027a8cbb86
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-15 16:42:57 +0700

    Update promote.js

--- plugins/promote.js
+++ plugins/promote.js
@@ -1,4 +1,8 @@
-let handler = async (m, { conn, participants }) => {
+let handler = async (m, { conn, isOwner, isAdmin, participants }) => {
+  if (!(isAdmin || isOwner)) {
+                global.dfail('admin', m, conn)
+                throw false
+                }
   let members = participants.filter(member => !member.isAdmin).map(member => member.jid)
   let users = m.mentionedJid.filter(user => members.includes(user))
   for (let user of users) await conn.groupMakeAdmin(m.chat, [user]).catch(console.log)
@@ -10,7 +14,6 @@ handler.command = /^(promote|admin|\^|↑)$/i
 
 handler.group = true
 
-handler.admin = true
 handler.botAdmin = true
 
 module.exports = handler
