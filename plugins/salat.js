461331707be2ebd2558a1a8d832261c900a0b3d2
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-16 10:36:22 +0700

    Update salat.js

--- plugins/salat.js
+++ plugins/salat.js
@@ -1,24 +1,25 @@
 const fetch = require('node-fetch')
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-    if (!text) return m.reply(`contoh:\n${usedPrefix + command} jakarta`)
+    if (!text) return m.reply(`contoh:\n${usedPrefix + command} pontianak`)
     let res = await fetch(global.API('zeks', '/api/jadwalsholat', { daerah: text }, 'apikey'))
     if (!res.ok) throw await `${res.status} ${res.statusText}`
     let json = await res.json()
     if (!json.status) {
         if (json.message == 'use of apikey reached the limit') throw json
-        let hasil = json.listdaerah.map((v, i) => `│ ${i + 1}. ${v}`).join`\n`
+        let hasil = json.listdaerah.map((v, i) => `┃ ${i + 1}. ${v}`).join`\n`
         m.reply(`
 *${json.message}*
 
 contoh:
-${usedPrefix + command} jakarta
+${usedPrefix + command} pontianak 
 
-┌ *Daftar Daerah*
-│ 
-${hasil}
-│ 
-└────`.trim())
+╭━━•✵ ⃟  ⃟  ⃟✵•
+┃
+┃ ુོ➪ *Daftar Daerah*
+${hasil} 
+┃ *─★‧ﾟ
+╰━━━━━━━━⸙`.trim())
         throw false
     }
     m.reply(`Jadwal Sholat ${text}\n\n${json.data.string}`.trim())
