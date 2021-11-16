e294739971e6343c7c8011f3579672ec8bd776cd
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-17 06:31:05 +0700

    Update stickergif.js

--- plugins/stickergif.js
+++ plugins/stickergif.js
@@ -48,7 +48,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
             .on('end', function () {
                 console.log('Finish')
                 buff = fs.readFileSync(ran)
-                conn.sendMessage(m.chat, buff, 'stickerMessage', { quoted: m, global.packname })
+                conn.sendMessage(m.chat, buff, 'stickerMessage', global.packname, global.author { quoted: m })
                 fs.unlinkSync(media)
                 fs.unlinkSync(ran)
             })
