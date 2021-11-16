c9771b696831288cb17a0fca3c49199532bdbc2d
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-16 09:08:04 +0700

    Update readmore.js

--- plugins/readmore.js
+++ plugins/readmore.js
@@ -4,9 +4,9 @@ let handler = async (m, { conn, text }) => {
   if (!r) r = ''
   conn.reply(m.chat, l + readMore + r, m)
 }
-handler.help = ['readmore','spoiler'].map(v => v + ' <teks>|<teks>')
+handler.help = ['readmore'].map(v => v + ' <teks>|<teks>')
 handler.tags = ['tools']
-handler.command = /^(spoiler|hidetext|readmore|selengkapnya)$/i
+handler.command = /^(spoiler|hidetext|(read)?more|selengkapnya)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
