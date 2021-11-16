3f4f04c1997a4d3077ee62f93b41348249b6cb5c
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-16 14:19:51 +0700

    Update setbotname.js

--- plugins/setbotname.js
+++ plugins/setbotname.js
@@ -11,7 +11,7 @@ let handler = async (m, { conn, text }) => {
 }
 handler.help = ['setbotname <teks>']
 handler.tags = ['owner']
-handler.command = /^(setbotname)$/i
+handler.command = /^(set(botname|nam(a)?e?bot))$/i
 handler.owner = true
 
 module.exports = handler
