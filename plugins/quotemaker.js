1aef204ebcb7dbbe3d51259fc888eb85685b1ee7
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-16 09:06:14 +0700

    Update quotemaker.js

--- plugins/quotemaker.js
+++ plugins/quotemaker.js
@@ -1,5 +1,6 @@
 let handler = async (m, { conn, text }) => {
   let [teks, wm] = text.split`|`
+  if (!teks) throw `Ketik ${usedPrefix + command} <teks>|<wm>`
   await conn.sendFile(m.chat, global.API('xteam', '/quotemaker', { text: teks, wm: wm ? wm : conn.getName(m.sender) }, 'APIKEY'), 'file.jpg', watermark, m, 0, { thumbnail: Buffer.alloc(0) })
 }
 handler.help = ['quote'].map(v => v + 'maker <teks>|<wm>')
