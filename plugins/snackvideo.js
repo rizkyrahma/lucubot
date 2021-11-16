4e2eb38c26c16aa5c4b6eb4e5983fd816531675a
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-17 02:33:46 +0700

    Update snackvideo.js

--- plugins/snackvideo.js
+++ plugins/snackvideo.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args }) => {
   global.API('xteam', '/dl/', {
     url: args[0]
   }, 'APIKEY')
-  conn.sendFile(m.chat, url, '', watermark, m)
+  conn.sendFile(m.chat, url, '', '', m)
 }
 handler.help = ['snackvideo'].map(v => v + ' <url>')
 handler.tags = ['downloader']
