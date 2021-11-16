cd830ce0f7ff0ef0893319abfda44227cb0de2ce
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-16 19:49:34 +0700

    Update report.js

--- plugins/report.js
+++ plugins/report.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw `kalo kamu nemu pesan eror, lapor pake perintah ini\n\ncontoh:\n${usedPrefix + command} selamat siang owner, sy menemukan eror seperti berikut <copy/tag pesan erornya>`
+    if (!text) throw `kalo kamu mau kirim pesan ke owner pake perintah ini\n\ncontoh:\n${usedPrefix + command} pesan kamu, jika ingin sertakan teks panjangeri <copy/tag pesannya>`
     if (text.length < 10) throw `Laporan terlalu pendek, minimal 10 karakter!`
     if (text.length > 1000) throw `Laporan terlalu panjang, maksimal 1000 karakter!`
     let teks = `*${command.toUpperCase()}!*\n\nDari : *@${m.sender.split`@`[0]}*\n\nPesan : ${text}\n`
