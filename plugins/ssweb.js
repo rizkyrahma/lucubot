f9782bee95e76a7d614c9d04382cc8e353d4497b
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-17 02:47:02 +0700

    Update ssweb.js

--- plugins/ssweb.js
+++ plugins/ssweb.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, command, args }) => {
   if (!args[0]) throw `uhm.. urlnya mana?`
   let url = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
   let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url, full }))).buffer()
-  conn.sendFile(m.chat, ss, 'screenshot.png', url, m, 0, { thumbnail: thumbfoto })
+  conn.sendFile(m.chat, ss, 'screenshot.png', url, m, 0, { thumbnail: await(await fetch(thumbfoto)).buffer() })
 }
 handler.help = ['ss'].map(v => v + ' <url>')
 handler.tags = ['internet']
