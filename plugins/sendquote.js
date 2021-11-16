471e17dae9a9773f2a019eec7dccaed45a5865bc
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-16 11:26:06 +0700

    Update sendquote.js

--- plugins/sendquote.js
+++ plugins/sendquote.js
@@ -2,7 +2,7 @@ async function handler(m) {
     if (!m.quoted) throw 'balas pesan!'
     let q = this.serializeM(await m.getQuotedObj())
     if (!q.quoted) throw 'pesan yang anda balas tidak mengandung balasan!'
-    await q.quoted.copyNForward(m.chat, null)
+    await q.quoted.copy(m.chat, true)
 }
 handler.command = /^q$/i
 module.exports = handler
