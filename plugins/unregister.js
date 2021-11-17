81904bd451fa127812b16475b96661025d0c9c4e
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-18 05:47:46 +0700

    Update unregister.js

--- plugins/unregister.js
+++ plugins/unregister.js
@@ -1,9 +1,9 @@
 const { createHash } = require('crypto')
 let handler = async function (m, { args }) {
-  if (!args[0]) throw 'Serial Number kosong'
+  if (!args[0]) throw 'SN/Serial Number kosong'
   let user = global.db.data.users[m.sender]
   let sn = createHash('md5').update(m.sender).digest('hex')
-  if (args[0] !== sn) throw 'Serial Number salah'
+  if (args[0] !== sn) throw 'SN/Serial Number salah'
   user.registered = false
   m.reply(`Unreg berhasil!`)
 }
