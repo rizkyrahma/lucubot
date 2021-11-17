be760febcf8efcd1616e1d7feb72d3f71beb4aea
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-18 06:09:37 +0700

    Update upload.js

--- plugins/upload.js
+++ plugins/upload.js
@@ -4,7 +4,7 @@ const uploadImage = require('../lib/uploadImage')
 let handler = async (m) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw 'Tidak ada media yang ditemukan'
+  if (!mime) throw 'Medianya mana?\nbokep juga bisa<3'
   let media = await q.download()
   let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
   let link = await (isTele ? uploadImage : uploadFile)(media)
@@ -14,6 +14,6 @@ ${isTele ? '(Tidak Ada Tanggal Kedaluwarsa)' : '(Tidak diketahui)'}`)
 }
 handler.help = ['upload']
 handler.tags = ['tools']
-handler.command = /^upload$/i
+handler.command = /^upload|to(ur)?l(ink)?$/i
 
 module.exports = handler
