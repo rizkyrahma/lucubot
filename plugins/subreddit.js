353f06eefdc8b2eeca74be66fb8215d3c7b35c09
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-17 21:24:48 +0700

    Update subreddit.js

--- plugins/subreddit.js
+++ plugins/subreddit.js
@@ -5,9 +5,9 @@ let handler = async (m, { conn, text }) => {
   if (!res.ok) throw eror
   let json = await res.json()
   if (!json.url) throw 'Media tidak ditemukan!'
-  if (json.nsfw && !db.data.settings.nsfw) throw 'Mode NSFW tidak aktif'
+  //if (json.nsfw && !db.data.settings.nsfw) throw 'Mode NSFW tidak aktif'
 
-  await conn.sendFile(m.chat, json.url, text, json.title, m, false, { thumbnail: await (await fetch(json.url)).buffer() })
+  await conn.sendFile(m.chat, json.url, text, json.title, m, false, { thumbnail: await (await fetch(thumbfoto)).buffer() })
 }
 handler.help = ['subreddit <pencarian>']
 handler.tags = ['internet']
