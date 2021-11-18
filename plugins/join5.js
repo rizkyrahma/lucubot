7619b7e81865e7d934c07de28c9c0f1a498d1ae4
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-18 11:22:13 +0700

    Add files via upload

---
+++ plugins/join5.js
@@ -0,0 +1,31 @@
+let { MessageType } = require('@adiwajshing/baileys')
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
+
+let handler = async (m, { conn, text, usedPrefix }) => {
+    let [_, code] = text.match(linkRegex) || []
+    if (!code) throw 'Link Salah'
+    let res = await conn.acceptInvite(code)
+    m.reply(`Berhasil join grup ${res.gid}`).then(() => {
+        var jumlahHari = 86400000 * 0.2
+        var now = new Date() * 1
+        if (now < global.db.data.chats[res.gid].expired) global.db.data.chats[res.gid].expired += jumlahHari
+        else global.db.data.chats[res.gid].expired = now + jumlahHari
+    })
+    await conn.sendMessage(res.gid, `Halo *${conn.user.name}*, saya adalah bot whatsapp yang dibangun dengan Nodejs, Saya baru saja bergabung di grup ini diundang oleh @${m.sender.split`@`[0]}
+    
+ketik *#menu* untuk melihat daftar perintah`, MessageType.text, { contextInfo: { externalAdReply :{
+       mediaUrl: ' ',
+       mediaType: 4,
+       title: 'rasel ×͜×',
+       body: 'Whatsapp Developer Bot',
+       thumbnailUrl: image,
+sourceUrl: 'https://wa.me/6285346545126?text=Assalamualaikum'
+}}})
+}
+handler.help = ['join5 <linkgrup> ']
+handler.tags = ['main', 'update']
+handler.command = /^join5$/i
+
+handler.limit = 5
+
+module.exports = handler
