43c36b3ee37529230c88091983972c38105ecda4
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-18 07:17:57 +0700

    Update vote-start.js

--- plugins/vote-start.js
+++ plugins/vote-start.js
@@ -1,8 +1,12 @@
-let handler = async (m, { conn, text, usedPrefix }) => {
+let handler = async (m, { conn, isOwner, isAdmin, text, usedPrefix }) => {
+    if (!(isAdmin || isOwner)) {
+                global.dfail('admin', m, conn)
+                throw false
+                }
     conn.vote = conn.vote ? conn.vote : {}
     let id = m.chat
     if (id in conn.vote) {
-        await conn.sendButton(m.chat, '_Masih ada vote di chat ini!_', '© stikerin', 'HAPUS VOTE', `${usedPrefix}hapusvote`, m)
+        await conn.sendButton(m.chat, '_Masih ada vote di chat ini!_', watermark, 'Hapus Vote', `${usedPrefix}hapusvote`, m)
         throw false
     }
     await conn.send2Button(m.chat, `Vote dimulai!
@@ -10,7 +14,7 @@ let handler = async (m, { conn, text, usedPrefix }) => {
 *${usedPrefix}upvote* - untuk ya
 *${usedPrefix}devote* - untuk tidak
 *${usedPrefix}cekvote* - untuk mengecek vote
-*${usedPrefix}hapusvote* - untuk menghapus vote`, '© stikerin', 'UPVOTE', `${usedPrefix}upvote`, 'DEVOTE', `${usedPrefix}devote`, m)
+*${usedPrefix}hapusvote* - untuk menghapus vote`, watermark, 'Upvote', `${usedPrefix}upvote`, 'Devote', `${usedPrefix}devote`, m)
     conn.vote[id] = [
         text,
         [],
@@ -22,5 +26,5 @@ handler.tags = ['vote']
 handler.command = /^(start|mulai)vote$/i
 handler.limit = true
 handler.group = true
-handler.admin = true
+
 module.exports = handler
