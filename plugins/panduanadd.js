48661c7f8eaa46e3df450980d450388343fb324d
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-18 23:36:02 +0700

    Update panduanadd.js

--- plugins/panduanadd.js
+++ plugins/panduanadd.js
@@ -6,13 +6,13 @@ Cara menambahkan bot ke dalam group.
 Lucu bot memberlakukan trial gratis, 
 Ada 3 opsi setiap opsi memiliki limit hingga 10 limit terpakai
 
-1. ${usedPrefix}join5 linkgrup 5jam (usrr) 
-2. ${usedPrefix}join12 linkgrup 12jam (user) 
-3. ${usedPrefix}join24 linkgrup 24jam (owner) 
+1. ${usedPrefix}join5 linkgrup 5 hours (user) 
+2. ${usedPrefix}join12 linkgrup 12 hours (user) 
+3. ${usedPrefix}join24 linkgrup 24 hours (owner) 
 
 Setelah waktu habis maka bot akan keluar dari group. 
 Untuk join Permanent silahkan ketik *${usedPrefix}sewa*
 `.trim(), watermark, 'Ok Saya Mengerti', 'rasel comel+ganteng no debat ok su<3', m)
 handler.command = /^join/i
-
+handler.private = true 
 module.exports = handler
