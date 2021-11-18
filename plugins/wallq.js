d0624ad5d7add9ee9a8f7c687901eedb336edca5
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-18 09:13:19 +0700

    Update wallq.js

--- plugins/wallq.js
+++ plugins/wallq.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch')
 
-let handler = async (m, { conn, usedPrefix, command, text }) => {
+let handler = async (m, { conn, args, usedPrefix, command, text }) => {
   if (!text) throw 'Nyari apa?'
   let res = await fetch(global.API('https://wall.alphacoders.com/api2.0', '/get.php', {
     auth: '3e7756c85df54b78f934a284c11abe4e',
@@ -10,7 +10,7 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
   if (!res.ok) throw eror
   let json = await res.json()
   let img = json.wallpapers[Math.floor(Math.random() * json.wallpapers.length)]
-  await conn.sendButtonImg(m.chat, await (await fetch(img)).buffer(), 'Wallpapernya bwank', watermark, 'Get Again', `${usedPrefix + command}`, m)
+  await conn.sendButtonImg(m.chat, await (await fetch(img.url_image)).buffer(), 'Wallpapernya bwank', watermark, 'Get Again', `${usedPrefix + command} ${args[0]}`, m)
   //await conn.sendFile(m.chat, img.url_image, 'wallpaper', '', m, 0, { thumbnail: Buffer.alloc(0) })
 }
 handler.help = ['wallpaperq <pencarian>']
