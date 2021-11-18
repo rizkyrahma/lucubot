833a988355d9313a871c0eb9bbc0d5ace0db840e
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-18 08:44:08 +0700

    Update wallpaperAnime.js

--- plugins/wallpaperAnime.js
+++ plugins/wallpaperAnime.js
@@ -5,7 +5,8 @@ let handler = async (m, { conn }) => {
     if (!res.ok) throw eror
     let img = await res.buffer()
     if (!img) throw img
-    conn.sendFile(m.chat, img, '', '© stikerin', m, 0, { thumbnail: await (await fetch(img)).buffer() })
+    await conn.sendButtonImg(m.chat, await (await fetch(img)).buffer(), 'Nekonya bwank', watermark, 'Get Again', '/wallpaper', m)
+    //conn.sendFile(m.chat, img, '', watermark, m, 0, { thumbnail: await (await fetch(img)).buffer() })
 }
 handler.help = ['wallpaperanime']
 handler.tags = ['internet']
