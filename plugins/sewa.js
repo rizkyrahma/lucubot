8ba8efe3082582e419485aa8f3898e5d7de31002
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-19 19:44:28 +0700

    Update sewa.js

--- plugins/sewa.js
+++ plugins/sewa.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let { MessageType } = require('@adiwajshing/baileys')
 let handler = async(m, { conn }) => {
-    let kontol = `╭━━•✵ ⃟  ⃟  ⃟✵•
+    let kontol = `╭━━━━•✵ ⃟  ⃟  ⃟✵•
 ┃
 ┃ *SEWA BOT : ↷↷*
 ┃ 
@@ -9,25 +9,21 @@ let handler = async(m, { conn }) => {
 ┃ *〲 ✅Dapatkan fitur khusus premium.
 ┃ *〲 ✅Masukkan bot ke group anda.
 ┃ *〲 ✅Limit UNLIMITED (Tidak Terbatas)
+┃ *⸙ ુོ➪ Jadibot & Sewa RDP
+┃ *〲 Bisa mengakses fitur owner:
+┃ *〲 1. Broadcast Bot 
+┃ *〲 2. Hidetag di grup
+┃ *〲 3. Run bot di RDP 24jam on
+┃ *〲 4. Bot fast respon
 ┃ *─★‧ﾟ
 ╰━━━━━━━━⸙
-Pembayaran:
-Dana: 085346545126
-OVO: 085346545126
-GOPAY: 085346545126
-PULSA: 085346545126 (+5.000)
-    
-「Syarat dan Ketentuan」
-1. Bot akan keluar jika sudah waktu nya keluar
-2. Jangan di kick
-3. Mematuhi syarat dan ketentuan bot pada #rules
-4. Di larang mengirimkan sesuatu yang mengganggu, contoh: virtex, berbagai macam bug.
-5. Di larang spam dalam penggunaan
+Pembayaran via:
+DANA, OVO, GOPAY
     `.trim()
     const button = {
         buttonText: 'List Harga',
         description: kontol,
-        sections:  [{title: "Silahkan di pilih 😇", rows: [
+        sections:  [{title: "Silahkan di pilih", rows: [
         {title: 'Premium', description: "Rp50.000\nDapatkan fitur Premium Permanent.", rowId:".konfir Saya ingin sewa + jadi premium"},
         {title: '4 Bulan', description: "Rp40.000\nSewa bot selama 4 bulan.", rowId:".konfir Saya ingin sewa bot 4 bulan"},
         {title: '3 Bulan', description: "Rp30.000\nSewa bot selama 3 bulan.", rowId:".konfir Saya ingin sewa bot 3 bulan"},
@@ -35,8 +31,8 @@ PULSA: 085346545126 (+5.000)
         {title: '1 Bulan', description: "Rp10.000\nSewa bot selama 1 bulan.", rowId:".konfir Saya ingin sewa bot 1 bulan"},
         {title: '2 Minggu', description: "Rp7.000\nSewa bot selama 2 minggu.", rowId:".konfir Saya ingin sewa bot 2 minggu"},
         {title: '1 Minggu', description: "Rp5.000\nSewa bot selama 1 minggu.", rowId:".konfir Saya ingin sewa bot 1 minggu"},
-        {title: '12 Jam', description: "Rp.0\nMemasukkan bot selama 12 Jam(-10 limit)", rowId:".joinf"},
-        {title: 'Sewa RDP', description: "Rp30.000 run bot 24jam selama 1 bulan ", rowId:".konfir Saya ingin sewa RDP-nya"},
+        {title: 'Jadibot sementara', description: "Syarat ketentuan owner", rowId:".konfir Saya ingin jadibot"},
+        {title: 'Sewa RDP', description: "Rp20.000 run bot 24jam selama 1 bulan ", rowId:".konfir Saya ingin sewa RDP-nya"},
         {title: 'Owner? ', description: "Chat owner nya jika ada perlu.", rowId:".owner"},
         {title: 'Join?', description: "Cara Memasukkan Bot Ke Grup.", rowId:".join"},
        ] }],
