207f5f235824c5256a9ac713fdc3f8017fe5f3d9
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-20 16:26:22 +0700

    Update join24.js

--- plugins/join24.js
+++ plugins/join24.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, text, usedPrefix }) => {
     let [_, code] = text.match(linkRegex) || []
     if (!code) throw 'Link Salah'
     let res = await conn.acceptInvite(code)
-    m.reply(`Berhasil join grup ${res.gid}`).then(() => {
+    m.reply(`Berhasil join grup ${res.gid} *BOT AKAN KELUAR DALAM WAKTU 24JAM*`).then(() => {
         var jumlahHari = 86400000 * 0.4
         var now = new Date() * 1
         if (now < global.db.data.chats[res.gid].expired) global.db.data.chats[res.gid].expired += jumlahHari
