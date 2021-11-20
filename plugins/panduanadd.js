1420b45abacd3005f0ce167869c7d4078fd5fbff
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-20 16:51:17 +0700

    Update panduanadd.js

--- plugins/panduanadd.js
+++ plugins/panduanadd.js
@@ -6,9 +6,11 @@ Cara menambahkan bot ke dalam group.
 Lucu bot memberlakukan trial gratis, 
 Ada 3 opsi setiap opsi memiliki limit hingga 10 limit terpakai
 
-1. ${usedPrefix}join5 linkgrup 5 hours (user) 
-2. ${usedPrefix}join12 linkgrup 12 hours (user) 
-3. ${usedPrefix}join24 linkgrup 24 hours (owner) 
+1. ${usedPrefix}join3 (3 hours) [user]
+2. ${usedPrefix}join5 (5 hours) user]
+3. ${usedPrefix}join12 (12 hours) [owner]
+4. ${usedPrefix}join24 (24 hours) [owner]
+5. ${usedPrefix}joinp (permanent) [owner]
 
 Setelah waktu habis maka bot akan keluar dari group. 
 Untuk join Permanent silahkan ketik *${usedPrefix}sewa*
