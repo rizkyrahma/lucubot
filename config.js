63f466ceba773a091d1df7e31e3c4639bbe6e4dc
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-20 07:43:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -70,6 +70,7 @@ Untuk 1 group
 
 Nb:bot ini masih terus dikembangkan.`
 global.ppkosong = 'https://telegra.ph/file/22fd84e4a3244e1b17e4e.png'
+global.ppgc = 'https://telegra.ph/file/22fd84e4a3244e1b17e4e.png'
 global.image = 'https://telegra.ph/file/c9a5e49b5336604baa137.jpg'//change the image
 global.thumbfoto = 'https://telegra.ph/file/c9a5e49b5336604baa137.jpg' //thumbnail foto tools
 global.yamete = 'https://telegra.ph/file/f7b13f18a1f21e5ca6aed.jpg' //qr
