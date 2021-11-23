e6537c5354cf54944aae7c08818df52db26bddae
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-11-23 20:00:15 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -410,7 +410,7 @@ module.exports = {
                 .setAvatar(pp)
                 .setBackground("https://i.ibb.co/KhtRxwZ/dark.png")
                 .toAttachment()
-              this.send2ButtonLoc(jid, action === 'add' ?wel.toBuffer() : lea.toBuffer(), text, watermark, 'Menu', '#menu', 'Welcome', 'rasel comel+ganteng no debat', null, {
+              this.send2ButtonLoc(jid, action === 'add' ?wel.toBuffer() : lea.toBuffer(), text, watermark, 'Ok', 'gak tau males pen beli truk 🗿', null, {
                 contextInfo: {
                   mentionedJid: [user]
                 }
